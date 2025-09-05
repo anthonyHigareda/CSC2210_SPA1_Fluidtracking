12725d4e9a0d5dec73c189a1ac1ac04e5726af62
higaredajra <higaredajra@msoe.edu>
2025-09-05 13:50:21 -0500

    Begin Stage1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,22 @@
 //
-// Created by higaredajra on 9/5/2025.
+// CSC2210-003
+// Dr. Hasker
+// Fall 2025
 //
+// Author: Anthony Higareda
+// SPA 1: Fluid Tracking
+// Created: 05 September 2025
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int fluidIntake = 0;
+  int fluidOutput = 0;
+
+  string outputFluids[] = ["urine", "bloodloss", "diarrhea"];
+
+
+}
