35b195f60fda66802776591636a793e0f013d497
higaredajra <higaredajra@msoe.edu>
2025-09-08 14:02:57 -0500

    Begin fluid differential logic

--- main.cpp
+++ main.cpp
@@ -17,8 +17,10 @@ using namespace std;
 
 int main() {
 
-  string time, fluidType;   // Time of fluid intake/output and the type of fluid
-  int fluidVolume, totalIntake, totalOutput; // Total amounts of fluid intake and output
+  string time, fluidType; // Time of fluid intake/output and the type of fluid
+  int fluidVolume;        // Volume of fluid taken from the file
+  int totalIntake = 0;    // Total amounts of fluid intake and output
+  int totalOutput = 0;
 
   //  https://www.geeksforgeeks.org/dsa/how-to-check-if-an-array-contains-a-value-or-not/
   //  Referenced this GFG article to use a vector for array searching
@@ -40,11 +42,17 @@ int main() {
     //  https://www.geeksforgeeks.org/cpp/check-if-vector-contains-given-element-in-cpp/
     //  Referenced this GFG article for help finding a matching output fluid name
     //  in the outputFluids vector.
-    //  If count() evaluates to 1, then the fluid we have pulled from the file
-    //  is considered and output fluid.
+    //  If count() evaluates to be nonzero, then the fluid
+    //  we have pulled from the file is an output fluid.
     if (count(begin(outputFluids), end(outputFluids), fluidType) > 0) {
+      totalOutput += fluidVolume;
+    } else {
+      totalIntake += fluidVolume;
+    }
 
-
+    if (totalIntake >= totalOutput + 1000) {
+      cout << "after consuming " << fluidType << " at " << time <<
+        ", intake exceeds output by " << totalIntake - totalOutput << " ml" << endl;
     }
 
   }
