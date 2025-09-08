91fa42269e2964f12f522f5e4c8bd1a94139aed8
higaredajra <higaredajra@msoe.edu>
2025-09-08 13:49:44 -0500

    Begin data processing

--- main.cpp
+++ main.cpp
@@ -10,15 +10,21 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <algorithm>
 using namespace std;
+
 
 int main() {
 
   string time, fluidType;   // Time of fluid intake/output and the type of fluid
-  int fluidIntake, fluidOutput; // Total amounts of fluid intake and output
+  int fluidVolume, totalIntake, totalOutput; // Total amounts of fluid intake and output
 
-  string outputFluids[] = {"urine", "bloodloss", "diarrhea"};
-  // The 3 types of fluid that can be outputs
+  //  https://www.geeksforgeeks.org/dsa/how-to-check-if-an-array-contains-a-value-or-not/
+  //  Referenced this GFG article to use a vector for array searching
+  //  Will make identifying output fluids much easier later on
+  //  The 3 types of fluid that can be outputs
+  vector<string> outputFluids{"urine", "bloodloss", "diarrhea"};
 
   ifstream file("data.txt");  // Open the input data text file.
   // NOTE: the data file must be inside "\cmake-build-debug" for whatever
@@ -28,8 +34,20 @@ int main() {
     cout << "File could not be opened" << endl;
   }
 
-  file >> time >> fluidType >> fluidIntake; // Pulls from the file stream and assigns the values
-  cout << "the final fluid differential is " << fluidIntake << " ml" << endl; // Report to the user
+  while (file >> time >> fluidType >> fluidVolume) {  // Pulls from the file stream and assigns values
+    // to time, fluidType, and fluidVolume, then keeps iterating until the file stream is empty.
+
+    //  https://www.geeksforgeeks.org/cpp/check-if-vector-contains-given-element-in-cpp/
+    //  Referenced this GFG article for help finding a matching output fluid name
+    //  in the outputFluids vector.
+    //  If count() evaluates to 1, then the fluid we have pulled from the file
+    //  is considered and output fluid.
+    if (count(begin(outputFluids), end(outputFluids), fluidType) > 0) {
+
+
+    }
+
+  }
 
   return 0;
 
